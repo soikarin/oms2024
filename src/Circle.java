9cd5f3e96109d76295f645ea49a60afbc64693ee
Santeri Oikarinen <santeri.oikarinen@student.oulu.fi>
2024-10-22 12:18:26 +0300

    Circle.java tehty

--- src/Circle.java
+++ src/Circle.java
@@ -1,1 +1,18 @@
+public class Circle extends Visibility implements ShapeBasic {
 
+    @Override
+    public void draw() {
+        System.out.println("This is a circle");
+    }
+
+    @Override
+    public void grow() {
+        System.out.println("Growing the size of the circle");
+    }
+
+    @Override
+    public String printText() {
+        return "Circle";
+    }
+
+}
