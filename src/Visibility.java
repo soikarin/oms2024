317ba94243bf6dec7e3d6a185f74c2673043b6aa
Santeri Oikarinen <santeri.oikarinen@student.oulu.fi>
2024-10-22 11:55:04 +0300

    Visibility.java tehty

--- src/Visibility.java
+++ src/Visibility.java
@@ -1,1 +1,16 @@
+abstract class Visibility {
+    private boolean inBackground = false;
 
+    public void changeVisibility() {
+        if (inBackground == false) {
+            inBackground = true;
+        } else {
+            inBackground = false;
+        }
+    }
+
+    public boolean isVisible() {
+        return inBackground;
+    }
+
+}
