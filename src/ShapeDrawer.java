1e940650cad563e089bd2ca0dc211ca179ffd27b
Santeri Oikarinen <santeri.oikarinen@student.oulu.fi>
2024-10-22 18:36:11 +0300

    ShaperDrawer.java kesken

--- src/ShapeDrawer.java
+++ src/ShapeDrawer.java
@@ -1,1 +1,9 @@
+public class ShapeDrawer extends Drawing {
 
+    @Override
+    public void draw() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+    
+}
