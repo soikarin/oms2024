f0116f55165a977920b60c8b0474987ef312774b
Santeri Oikarinen <santeri.oikarinen@student.oulu.fi>
2024-10-22 12:21:08 +0300

    Rectangle.java tehty

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,1 +1,18 @@
+public class Rectangle extends Visibility implements ShapeBasic {
 
+    @Override
+    public void draw() {
+        System.out.println("This is a rectangle");
+    }
+
+    @Override
+    public void grow() {
+        System.out.println("Growing the size of the rectangle");
+    }
+
+    @Override
+    public String printText() {
+        return "Rectangle";
+    }
+
+}
