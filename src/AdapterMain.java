d931904ce93a017a562577dfc40eb74eb07f7475
Santeri Oikarinen <santeri.oikarinen@student.oulu.fi>
2024-10-29 15:04:59 +0200

    AdapterMain valmis

--- src/AdapterMain.java
+++ src/AdapterMain.java
@@ -2,7 +2,27 @@
 public class AdapterMain {
 
     public static void main(String[] args) {
-        //TODO: main funktio
+        ShapeDrawer drawer = new ShapeDrawer();
+
+        Rectangle visibleRectangle1 = new Rectangle();
+        drawer.addBasicShape(visibleRectangle1);
+        Rectangle backroundRectangle = new Rectangle();
+        backroundRectangle.changeVisibility();
+        drawer.addBasicShape(backroundRectangle);
+
+        Circle visibleCircle = new Circle();
+        drawer.addBasicShape(visibleCircle);
+        Circle backroundCircle = new Circle();
+        backroundCircle.changeVisibility();
+        drawer.addBasicShape(backroundCircle);
+
+        Rectangle visibleRectangle2 = new Rectangle();
+        drawer.addBasicShape(visibleRectangle2);
+
+        drawer.draw();
+        drawer.grow();
+        drawer.showVisibility();
+
     }
 
 }
